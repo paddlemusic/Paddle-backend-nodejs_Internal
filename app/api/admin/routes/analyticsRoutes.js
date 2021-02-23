aad090fcf1cd27dd7675cd2ce2f7de0234830262
Simnan Khan <simnan.khan@algoworks.com>
2021-02-23 11:56:59 +0530

    updated changes

--- app/api/admin/routes/analyticsRoutes.js
+++ app/api/admin/routes/analyticsRoutes.js
@@ -3,5 +3,5 @@ const router = express.Router()
 const AnalyticsController = require('../controllers/analyticsController')
 const analyticsController = new AnalyticsController()
 router.get('/getTotalStreams', analyticsController.getStreamsTotally)
-router.get('/getMonthlyStreams', analyticsController.getStreamsMonthly)
+// router.get('/getMonthlyStreams', analyticsController.getStreamsMonthly)
 module.exports = router
